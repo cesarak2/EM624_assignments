a23a7dbd144ad9addd8df69f8adc4d83ec893924
Cesar <cesarak2+github@gmail.com>
2018-02-16 11:12:03 -0500

    Modified W03 submit file
    Running an if statement in all lines of a file containing 8 million lines makes no visible difference in execution time.
    Changed the program to verify the lines to be printed within each iteration.

--- W03/Submit/CountNames.py
+++ W03/Submit/CountNames.py
@@ -7,25 +7,38 @@
 
 
 
+# countLines = 0
+# with open("marketingdata.txt") as marketingData:
+#     lineContent = marketingData.readline()
+#     while lineContent:
+#         # lineContent = lineContent.strip()
+#         countLines += 1
+#         lineContent = marketingData.readline()
+#     print countLines
+# marketingData.close()
+#
+# with open("marketingdata.txt") as marketingData:
+#     printLine = 0
+#     print "\nThese are the first five lines in the file: \n"
+#     lineContent = marketingData.readline()
+#     while lineContent:
+#         lineContent.strip()
+#         printLine += 1
+#         if printLine <= 5:
+#             print lineContent.strip()
+#         lineContent = marketingData.readline()
+#
+# print countLines
+
+
 countLines = 0
+print "\nThese are the first five lines in the file: \n"
 with open("marketingdata.txt") as marketingData:
     lineContent = marketingData.readline()
     while lineContent:
         # lineContent = lineContent.strip()
         countLines += 1
-        lineContent = marketingData.readline()
-    print countLines
-marketingData.close()
-
-with open("marketingdata.txt") as marketingData:
-    printLine = 0
-    print "\nThese are the first five lines in the file: \n"
-    lineContent = marketingData.readline()
-    while lineContent:
-        lineContent.strip()
-        printLine += 1
-        if printLine <= 5:
+        if countLines <= 5:
             print lineContent.strip()
         lineContent = marketingData.readline()
-
-print countLines
+    print "\nThe file has %s lines" % countLines
