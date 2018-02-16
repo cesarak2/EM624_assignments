16b7529f9acaa6c2f6e7ba448206e78ece259da3
Cesar <cesarak2+github@gmail.com>
2018-02-16 11:30:17 -0500

    Modified W03 submit file
    Added comments to the first part

--- W03/Submit/CountNames.py
+++ W03/Submit/CountNames.py
@@ -30,15 +30,28 @@
 #
 # print countLines
 
+# Part 1:
+# processing a text file
 
+
+# Set the counter of lines as 0
 countLines = 0
 print "\nThese are the first five lines in the file: \n"
-with open("marketingdata.txt") as marketingData:
+# Importing the file as marketingData as read only.
+with open("marketingdata.txt", 'r') as marketingData:
+    # extracting the value of that line
     lineContent = marketingData.readline()
+    # a loop between all lines to count the number of lines
     while lineContent:
-        # lineContent = lineContent.strip()
         countLines += 1
+        # Verifies if the loop is running in the first 5 lines. If yes, print the
+        # content of that line without the break line using .strip()
         if countLines <= 5:
             print lineContent.strip()
+        # read the next line. When there is no more lines, break the loop.
         lineContent = marketingData.readline()
+    # print how many lines the file has.
     print "\nThe file has %s lines" % countLines
+
+# Part 2:
+# processing a csv file.
