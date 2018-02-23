078252372d5a85fee247dea9f6d98c3a43afc7d4
Cesar <cesarak2+github@gmail.com>
2018-02-23 18:41:51 -0500

    Working on W04
    Working on average mileage traveled

--- W04 – Functions and modules/Submit/CityBikeNY.py
+++ W04 – Functions and modules/Submit/CityBikeNY.py
@@ -31,8 +31,10 @@ def storeFileInList(filetoberead):
 
 
 def printDetailsMonth(listCiti):
-    print "The following data is from"
-    print
+    pass
+    # print "The following data is from"
+
+
 
 
 
@@ -43,14 +45,53 @@ CitiBike = importFile("citi_bike.txt")
 dataCitiBikeList = storeFileInList(CitiBike)
 
 
+# # Count the number of lines
+# count = 0
+# for line in dataCitiBikeList:
+#     count +=1
+# print "number of lines of the file is: %d" % count
+
+
 
 last = len(dataCitiBikeList)
 last = int(last) - 1
 # print dataCitiBikeList
 print last
-print dataCitiBikeList[last][0]
+print "First: ", dataCitiBikeList[0][0], "\t\t\tlast: ", dataCitiBikeList[last][0]
 # print "The file CitiBike.csv has %s lines, of which %s is/are from 1/15/2014" % (nLinesCSVFile, countLinesMatchDate)
 
 
+# Print all 1 columns from all lists
+# i = 0
+# for line in dataCitiBikeList:
+#     print dataCitiBikeList[i][0]
+#     i += 1
 
 
+i = 0
+for line in dataCitiBikeList:
+    if dataCitiBikeList[i][0].startswith("6/"):
+        print dataCitiBikeList[i][3]
+    i += 1
+
+
+
+
+    i = 0
+    j = 0
+    monthSumMileageTraveled = 0
+    for line in dataCitiBikeList:
+        if dataCitiBikeList[i][0].startswith("6/"):
+            monthSumMileageTraveled += dataCitiBikeList[i][3]
+            j += 1
+        i += 1
+    averageMileageTraveled = monthSumMileageTraveled / j
+
+
+
+
+
+    # juneTraveledMiles = []
+    # line = dataCitiBikeList
+    # if line.startswith("6/"):
+    #     print line
